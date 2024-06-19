38c3c52021047c4b35e8abe6095a14d4b63eabc6
Taher Mahmood <taher.ccg@gmail.com>
2024-06-19 15:48:13 +0000

    Add: Logout funcionality

--- app.py
+++ app.py
@@ -92,6 +92,14 @@ def profile(username):
     return render_template("profile.html", username=username)
 
 
+@app.route("/logout")
+def logout():
+    # Remove user session (logout)
+    flash("You have logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
